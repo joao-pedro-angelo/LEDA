82caaeacd4af61abba811c7cb6703cb52a67df4f
João Pedro Carneiro Angelo <129865329+joao-pedro-angelo@users.noreply.github.com>
2023-07-03 13:32:23 -0300

    Roteiro 03

--- R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,37 +12,55 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		if (array.length > 1 && leftIndex < rightIndex && rightIndex < array.length) {
-			int max = CountingSort.getMax(array, leftIndex, rightIndex);
-			int min = CountingSort.getMin(array, leftIndex, rightIndex);
-			int size = max - min + 1;
+		if (array != null && leftIndex >= 0 && leftIndex < rightIndex && rightIndex < array.length) {
+			int maior = maior(array, leftIndex, rightIndex);
+			int menor = menor(array, leftIndex, rightIndex);
 
-			int[] C = new int[size];
+			int[] C = new int[maior - menor + 1];
 
 			for (int i = leftIndex; i <= rightIndex; i++) {
-				int number = array[i];
-				int position = number - min;
-
-				C[position] += 1;
+				C[array[i] - menor] += 1;
 			}
 
 			for (int i = 1; i < C.length; i++) {
-				C[i] = C[i] + C[i - 1];
+				C[i] += C[i - 1];
 			}
 
-			int[] B = new int[array.length];
-			for (int i = 0; i < array.length; i++) {
-				B[i] = array[i];
-			}
+			int[] B = new int[rightIndex - leftIndex + 1];
 
 			for (int i = rightIndex; i >= leftIndex; i--) {
-				int number = B[i];
-				int j = number - min;
-				C[j]--;
-				int position = C[j] + leftIndex;
-				array[position] = number;
+				B[C[array[i] - menor] - 1] = array[i];
+				C[array[i] - menor] -= 1;
+			}
+
+			int indice = 0;
+			for (int i = leftIndex; i <= rightIndex; i++) {
+				array[i] = B[indice];
+				indice++;
+
 			}
 		}
+		
+	}
+
+	private int maior(Integer[] array, int leftIndex, int rightIndex) {
+		int maior = array[leftIndex];
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			if (array[i] > maior) {
+				maior = array[i];
+			}
+		}
+		return maior;
+	}
+
+	private int menor(Integer[] array, int leftIndex, int rightIndex) {
+		int menor = array[leftIndex];
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			if (array[i] < menor) {
+				menor = array[i];
+			}
+		}
+		return menor;
 	}
 
 }
